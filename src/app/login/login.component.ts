500c3faf7e85ebb0ebb6905dcadb0711eafd7297
Lars Schumacher <schumacher90lars@gmail.com>
2025-03-15 00:53:10 +0100

    login mockup done & global scss files & variables added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  isLocked: boolean = true;
+  email: string = '';
+  password: string = '';
 
+  toggleLock(): void {
+    this.isLocked = !this.isLocked;
+  }
 }
