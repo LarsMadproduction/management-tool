26f2b9cd9d2d04290d32be58fa162526efb8f419
Lars Schumacher <schumacher90lars@gmail.com>
2025-03-16 00:18:40 +0100

    added components

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "../shared/header/header.component";
+import { SidenavComponent } from "../shared/sidenav/sidenav.component";
+
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, HeaderComponent, SidenavComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
