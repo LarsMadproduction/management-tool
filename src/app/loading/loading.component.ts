f97552f0c0ca2761b69c2bc50e5c4b45bd6e8be5
Lars Schumacher <schumacher90lars@gmail.com>
2025-03-15 22:59:52 +0100

    added laoding animation

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loading',
   standalone: true,
   imports: [],
   templateUrl: './loading.component.html',
-  styleUrl: './loading.component.scss'
+  styleUrl: './loading.component.scss',
 })
-export class LoadingComponent {
+export class LoadingComponent implements OnInit {
+  isAnimated = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.isAnimated = true;
+    }, 100);
+
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 3000);
+  }
 }
