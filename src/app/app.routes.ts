9feb37820bd6cd4eda72b366dfa36f889c671a9b
Lars Schumacher <schumacher90lars@gmail.com>
2025-03-15 21:19:35 +0100

    added new components and architecture

---
+++ src/app/app.routes.ts
@@ -0,0 +1,33 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'loading', pathMatch: 'full' },
+  { 
+    path: 'loading', 
+    loadComponent: () => import('./loading/loading.component').then(m => m.LoadingComponent) 
+  },
+  { 
+    path: 'login', 
+    loadComponent: () => import('./login/login.component').then(m => m.LoginComponent) 
+  },
+  {
+    path: 'app',
+    loadComponent: () => import('./layout/layout.component').then(m => m.LayoutComponent),
+    children: [
+      { path: '', redirectTo: 'news', pathMatch: 'full' },
+      { 
+        path: 'news', 
+        loadComponent: () => import('./features/news/news.component').then(m => m.NewsComponent) 
+      },
+      { 
+        path: 'profile', 
+        loadComponent: () => import('./features/participants/participants.component').then(m => m.ParticipantsComponent) 
+      },
+      { 
+        path: 'settings', 
+        loadComponent: () => import('./features/evaluation/evaluation.component').then(m => m.EvaluationComponent) 
+      },
+    ]
+  },
+  { path: '**', redirectTo: 'login' } 
+];
