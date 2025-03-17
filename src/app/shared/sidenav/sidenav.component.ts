84777411602201f1b4d72cb2dba725ace7448ff1
Lars Schumacher <schumacher90lars@gmail.com>
2025-03-17 15:17:55 +0100

    added list and sublists

--- src/app/shared/sidenav/sidenav.component.ts
+++ src/app/shared/sidenav/sidenav.component.ts
@@ -1,12 +1,62 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.scss'
+  styleUrl: './sidenav.component.scss',
 })
 export class SidenavComponent {
+  categoryList: string[] = [
+    'Neuigkeiten',
+    'Teilnehmer Listen',
+    'Bewertung',
+    'Gewerk',
+    'Firmen',
+  ];
 
+  years: string[] = ['2025', '2024', '2023', '2022', '2021', '2020'];
+
+  trades: string[] = [
+    'Mauerer',
+    'Betonbauer',
+    'Holzbauer',
+    'Straßenbauer',
+    'Wasserbauer',
+    'Rohrleitungsbauer',
+  ];
+
+  companies: string[] = ['Strabag', 'Eurovia', 'Kemna', 'Graf'];
+
+  expandedCategories: { [key: string]: boolean } = {};
+
+  toggleSublist(category: string): void {
+    if (this.isToggleCategory(category)) {
+      this.expandedCategories[category] = !this.expandedCategories[category];
+    }
+  }
+
+  getSublist(category: string): string[] {
+    if (category === 'Teilnehmer Listen') return this.years;
+    if (category === 'Gewerk') return this.trades;
+    if (category === 'Firmen') return this.companies;
+    return [];
+  }
+
+  openCategory(category: string): void {
+    console.log(`Kategorie "${category}" wurde geöffnet.`);
+  }
+
+  isToggleCategory(category: string): boolean {
+    return (
+      category === 'Teilnehmer Listen' ||
+      category === 'Gewerk' ||
+      category === 'Firmen'
+    );
+  }
+  openSubitem(subItem: string): void {
+    console.log(`Kategorie "${subItem}" wurde geöffnet.`);
+  }
 }
